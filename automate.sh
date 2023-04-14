1656a62d50254df5560e3403d8720b9554de1175
steven <steven841221@gmail.com>
2023-04-14 21:23:37 +1000

    feat: update automate sh file

--- automate.sh
+++ automate.sh
@@ -4,7 +4,11 @@ docker_hub_repo=ci-test
 docker_name=golang-pipeline
 
 echo "You are currently at branch of $branch"
-sed -i '' "s/CURRENT_VERSION: *.*.*/CURRENT_VERSION: $1/g" "s/DOCKER_HUB_REPO:"*"/DOCKER_HUB_REPO:"$docker_hub_repo"/g" "s/DOCKER_NAME:"*"/DOCKER_NAME:"$docker_name"/g" ./.github/workflows/cicd.yml
+sed -i '' "s/CURRENT_VERSION: *.*.*/CURRENT_VERSION: $1/g" ./.github/workflows/cicd.yml
+sed -i '' "s/DOCKER_HUB_REPO:"*"/DOCKER_HUB_REPO:"$docker_hub_repo"/g" ./.github/workflows/cicd.yml
+sed -i '' "s/DOCKER_NAME:"*"/DOCKER_NAME:"$docker_name"/g" ./.github/workflows/cicd.yml
+
+
 git add .
 git commit -m "$2"
 git push -u origin "$branch"
