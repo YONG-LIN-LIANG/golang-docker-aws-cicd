9d8aa3ae6099a323e0ecb13310fc3130e4d25f9d
steven <steven841221@gmail.com>
2023-04-14 18:45:27 +1000

    feat: update automate sh file

--- automate.sh
+++ automate.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+branch=$(git symbolic-ref --short HEAD)
 
+echo "You are currently at branch of $branch"
 sed -i '' "s/CURRENT_VERSION: *.*.*/CURRENT_VERSION: $1/g" ./.github/workflows/cicd.yml
 git add .
 git commit -m "$2"
-git push -u origin "$3"
+git push -u origin "$branch"
